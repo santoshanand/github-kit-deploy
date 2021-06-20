0c509295f112880b7e86810fc07cf0fa25fc1f3c
Santosh Anand <santoshanand.jogi@gmail.com>
2021-06-20 19:42:45 +0530

    Added path into invironment variable

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+	const PATH = import.meta.env.VITE_GITHUB_PATH as string;
 </script>
 <header class="text-gray-600 body-font shadow-sm">
 	<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-		<a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+		<a href={PATH} class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
 				<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
 			</svg>
 			<span class="ml-3 text-xl">Tailblock</span>
 		</a>
 		<nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-			<a href="/" class="mr-5 hover:text-gray-900">Home</a>
-			<a href="/projects" class="mr-5 hover:text-gray-900">Projects</a>
+			<a href={PATH} class="mr-5 hover:text-gray-900">Home</a>
+			<a href="{PATH}projects" class="mr-5 hover:text-gray-900">Projects</a>
 		</nav>
 	</div>
 </header>
